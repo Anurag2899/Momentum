41cb0ab0250987457b9c8c2ba74b1d1de9f85f84
Anurag2899 <kumaranu@groww.in>
2024-04-15 02:46:54 +0530

    time calc

--- main.py
+++ main.py
@@ -3,25 +3,30 @@ from dataAnalyzer.dataanalyser import DataAnalyser
 from momentumAnalyser.momentumanalyser import MomentumAnalyser
 from constants.constants import Constants
 from logger import logger
-
+import time
 
 def main():
+    start_time = time.time()
 
-    logger.infop("Starting data collection")
+    logger.info("Starting data collection")
     LOS = Constants.LIST_OF_ALL_STOCKS_INDIA
     HDSD = Constants.HISTORICAL_DATA_SAVE_DIR
     collector = DataCollector(LOS, HDSD)
     collector.collect_data()
 
-    logger.infop("Starting data analysis")
+    logger.info("Starting data analysis")
     ADSD = Constants.ANALYSED_DATA_SAVE_DIR
     analyser = DataAnalyser(HDSD, ADSD)
     analyser.process_all_data()
 
-    logger.infop("Starting MOMENTUM analysis")
+    logger.info("Starting MOMENTUM analysis")
     MDSD = Constants.MOMENTUM_DATA_SAVE_DIR
-    momentum = MomentumAnalyser(ADSD,MDSD)
+    momentum = MomentumAnalyser(ADSD, MDSD)
     momentum.process_all_data()
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    logger.info(f"Total time taken: {elapsed_time} seconds")
 
 if __name__ == "__main__":
     main()
